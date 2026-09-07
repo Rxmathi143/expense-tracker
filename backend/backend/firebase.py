80889a4d1e61085ecb018b5a31cc16340048210e
Mathivishnu <134252606+Rxmathi143@users.noreply.github.com>
2026-09-08 04:35:43 +0530

    Update firebase.py

--- backend/backend/firebase.py
+++ backend/backend/firebase.py
@@ -1,16 +1,25 @@
 import firebase_admin
 from firebase_admin import credentials, firestore
 import os
+import json
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-SERVICE_ACCOUNT_PATH = os.path.join(
-    BASE_DIR,
-    "serviceAccountKey.json"
-)
-
 if not firebase_admin._apps:
-    cred = credentials.Certificate(SERVICE_ACCOUNT_PATH)
+
+    if os.environ.get("FIREBASE_CREDENTIALS"):
+        firebase_credentials = json.loads(
+            os.environ.get("FIREBASE_CREDENTIALS")
+        )
+        cred = credentials.Certificate(firebase_credentials)
+
+    else:
+        SERVICE_ACCOUNT_PATH = os.path.join(
+            BASE_DIR,
+            "serviceAccountKey.json"
+        )
+        cred = credentials.Certificate(SERVICE_ACCOUNT_PATH)
+
     firebase_admin.initialize_app(cred)
 
 db = firestore.client()
